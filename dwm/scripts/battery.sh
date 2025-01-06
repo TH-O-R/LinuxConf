398d050c18ad3ba7921888815e00b3af86e1f570
salah <salaheddinebentaleb5@gmail.com>
2025-01-06 12:06:30 +0100

    added dwm config files!

---
+++ dwm/scripts/battery.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# battery.sh - A simple status script for dwm
+
+# Battery status
+battery=$(cat /sys/class/power_supply/BAT0/capacity)
+charging_status=$(cat /sys/class/power_supply/BAT0/status)
+
+echo " [$charging_status > $battery %]"
