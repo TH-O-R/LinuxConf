2a514e6e8ff9155d2556befda3d05b9407e002d9
TH-O-R <salaheddinebentaleb5@gmail.com>
2025-04-05 10:07:49 +0000

    added cool fox colorsheme to Neovim

--- .config/nvim/lua/plugins/colortheme.lua
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -1,16 +1,56 @@
+-- return {
+-- 	"uZer/pywal16.nvim",
+-- 	-- for local dev replace with:
+-- 	-- dir = '~/your/path/pywal16.nvim',
+-- 	config = function()
+-- 		vim.cmd.colorscheme("pywal16")
+-- 	end,
+-- }
+--
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.g.tokyonight_style = "night" -- Can be Night - Moon - Storm or Day
-    vim.g.tokyonight_transparent_sidebar = false
-    vim.g.tokyonight_transparent = false
-    vim.g.tokyonight_italic_comments = true
-    vim.g.tokyonight_italic_keywords = false
-    vim.g.tokyonight_italic_functions = false
-    vim.g.tokyonight_bold = true
+	-- "EdenEast/nightfox.nvim"
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.tokyonight_style = "night" -- Can be Night - Moon - Storm or Day
+			vim.g.tokyonight_transparent_sidebar = false
+			vim.g.tokyonight_transparent = false
+			vim.g.tokyonight_italic_comments = true
+			vim.g.tokyonight_italic_keywords = false
+			vim.g.tokyonight_italic_functions = false
+			vim.g.tokyonight_bold = true
 
-    vim.cmd("colorscheme tokyonight-night")
-  end,
+			-- Toggle background transparency
+			local tokyo = false
+
+			local toggle_color = function()
+				if tokyo then
+					vim.cmd("colorscheme carbonfox")
+				else
+					vim.cmd("colorscheme tokyonight-night")
+				end
+				tokyo = not tokyo
+			end
+
+			vim.keymap.set(
+				"n",
+				"<leader>bg",
+				toggle_color,
+				{ desc = "toggle colorscheme", noremap = true, silent = true }
+			)
+			vim.cmd("colorscheme tokyonight-night")
+		end,
+	},
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.nightfox_style = "carbon"
+			vim.g.nightfox_transparent = false
+			vim.cmd("colorscheme carbonfox")
+		end,
+	},
 }
